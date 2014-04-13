ee5a89497ffc18e82e5f19be6cabb87bc892c35d
Jason Teplitz <jteplitz602@gmail.com>
2014-04-12 22:32:47 -0700

    Made shitty query parser marginally better

--- utils.js
+++ utils.js
@@ -80,7 +80,7 @@ var utils = {};
       var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
       for(var i = 0; i < hashes.length; i++){
           hash          = hashes[i].split('=');
-          vars[hash[0]] = hash[1];
+          vars[hash[0]] = decodeURIComponent(hash[1]);
       }
       return vars;
   };
